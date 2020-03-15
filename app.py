b1a4bb210f70c857dbab1b6126dd012353d39d11
Michael <ono.michael@gmail.com>
2020-03-15 17:41:35 -0400

    added host

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def analyze():
 		return render_template('frisky.html',twitter = tweet, scores = score)
 
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.run(host = '0.0.0.0', port = 80, debug = True)
